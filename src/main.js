b893a9fd8fb9f53fcf5177a9296e10f63eb37d0c
Bearknight <867134884@qq.com>
2019-05-06 10:36:09 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// 导入elmUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+// 导入路由
+import router from "router";
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,18 @@
+// 导入vue
+import Vue from "vue"
+// 导入vue-router
+import VueRouter from "vue-router"
+Vue.use(VueRouter)
+
+// 导入组件
+
+// 路由规则
+const routes=[];
+
+// 实例化路由对象
+const router =new VueRouter({
+    routes
+})
+
+// 暴露出去
+export default router;
